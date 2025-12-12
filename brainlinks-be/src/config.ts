dbd2640d7977283d66c7ff954998ec2674d14c72
Aniket <aniketsingh2151@gmail.com>
2025-12-12 23:26:32 +0530

    be overhaul and prep for openai integration

--- brainlinks-be/src/config.ts
+++ brainlinks-be/src/config.ts
@@ -1,2 +1,9 @@
-export const JWT_PASSWORD = process.env.JWT_PASSWORD || "your-secret-key";
-export const MONGODB_URL = process.env.MONGODB_URL || "MONGOURL=mongodb://root:example@localhost:27017";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const MONGO_URL = process.env.MONGO_URL || "";
+export const JWT_PASSWORD = process.env.JWT_PASSWORD || "";
+export const OPENROUTER_API_KEY = process.env.OPENROUTER_API_KEY || "";
+export const PINECONE_API_KEY = process.env.PINECONE_API_KEY || "";
+export const PINECONE_INDEX = process.env.PINECONE_INDEX || "brainlinks";
+export const PORT = process.env.PORT || 3000;
